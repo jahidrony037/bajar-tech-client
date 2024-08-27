a829610c6f6f4f8e78c2726cb20d57b28b0b2677
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-08-27 17:27:36 +0600

    Home page Edited

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Product from "./Product";
+
 const Home = () => {
-  return <div>This is Home Page </div>;
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await axios.get(
+      `https://bajar-tech-server.vercel.app/products/random`
+    );
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-5 justify-items-center mt-40">
+        {products?.length ? (
+          products?.map((product) => (
+            <Product key={product._id} product={product} />
+          ))
+        ) : (
+          <p className="text-primary font-bold text-4xl">no Data Found</p>
+        )}
+      </div>
+
+      <Link to={"/products"} className="flex justify-center mt-3">
+        <button className="btn btn-primary">See All</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
